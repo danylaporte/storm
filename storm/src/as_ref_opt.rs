67b911e7621a7bf3c1e2d27db4f2b35b8f60894a
Dany Laporte <dany_laporte@hotmail.com>
2022-01-08 10:52:32 -0500

    Add FromRefOpt trait

--- storm/src/as_ref_opt.rs
+++ storm/src/as_ref_opt.rs
@@ -1,3 +1,16 @@
 pub trait AsRefOpt<T> {
     fn as_ref_opt(&self) -> Option<&T>;
 }
+
+pub trait FromRefOpt<U> {
+    fn from_ref_opt(u: &U) -> Option<&Self>;
+}
+
+impl<T, U> FromRefOpt<U> for T
+where
+    U: AsRefOpt<T>,
+{
+    fn from_ref_opt(u: &U) -> Option<&Self> {
+        u.as_ref_opt()
+    }
+}
