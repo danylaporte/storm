38d890ec836171330944b42b6e7ec5a71a47393b
Arthur MATTHYS <AMY@dimosoftware.com>
2022-07-07 16:26:23 +0200

    feat(Vectable): add len method

--- storm/src/vec_table.rs
+++ storm/src/vec_table.rs
@@ -31,6 +31,11 @@ impl<E: Entity> VecTable<E> {
         self.map.keys()
     }
 
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+
     fn update_metrics(&self)
     where
         E: CtxTypeInfo,
