3acdeacf779fefc8c59bc95c05641bd82ac7c7f6
danylaporte <danylaporte@users.noreply.github.com>
2023-12-19 19:50:47 +0000

    deploy: 7f3111aa5321a2d1efdb5e2862c93b29a71551e1

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"storm":[["impl&lt;Fields&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;"]]
+"storm":[["impl&lt;Fields&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
