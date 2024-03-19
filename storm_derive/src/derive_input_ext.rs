385ed8f7a29f1c2ad43d5b9f2ab67766e27a6b66
Dany Laporte <dany_laporte@hotmail.com>
2024-03-19 17:48:51 -0400

    remove fn tuple_single_field

--- storm_derive/src/derive_input_ext.rs
+++ storm_derive/src/derive_input_ext.rs
@@ -1,6 +1,6 @@
 //use crate::AttrsExt;
 use proc_macro2::TokenStream;
-use syn::{spanned::Spanned, Data, DeriveInput, Error, Field, Fields};
+use syn::{spanned::Spanned, Data, DeriveInput, Error, Fields};
 
 pub trait DeriveInputExt {
     fn input(&self) -> &DeriveInput;
@@ -10,21 +10,6 @@ pub trait DeriveInputExt {
         match &input.data {
             Data::Struct(s) => Ok(&s.fields),
             _ => Err(Error::new(input.span(), "Only struct are supported.").to_compile_error()),
-        }
-    }
-
-    fn tuple_single_field(&self) -> Result<&Field, TokenStream> {
-        let fields = self.fields()?;
-
-        let mut iter = fields.into_iter();
-
-        match (iter.next(), iter.next()) {
-            (Some(f), None) if f.ident.is_none() => Ok(f),
-            _ => Err(Error::new(
-                self.input().span(),
-                "Only tuple with one field are supported.",
-            )
-            .to_compile_error()),
         }
     }
 }
