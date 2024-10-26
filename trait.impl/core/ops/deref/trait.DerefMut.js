7feecc599f04b60928bcdd536ab2f8d00e7c5622
danylaporte <danylaporte@users.noreply.github.com>
2024-10-26 02:27:55 +0000

    deploy: 009dfe0c4c0d9042b0cc4dbead9b118e61098782

--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["storm_mssql",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"storm_mssql/struct.MssqlTransactionGuard.html\" title=\"struct storm_mssql::MssqlTransactionGuard\">MssqlTransactionGuard</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["storm_mssql",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"storm_mssql/struct.MssqlTransactionGuard.html\" title=\"struct storm_mssql::MssqlTransactionGuard\">MssqlTransactionGuard</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[355]}
+//{"start":57,"fragment_lengths":[345]}
--- trait.impl/tiberius/to_sql/trait.ToSql.js
+++ trait.impl/tiberius/to_sql/trait.ToSql.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["storm_mssql",[["impl&lt;'a&gt; ToSql for <a class=\"struct\" href=\"storm_mssql/struct.Parameter.html\" title=\"struct storm_mssql::Parameter\">Parameter</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["storm_mssql",[["impl ToSql for <a class=\"struct\" href=\"storm_mssql/struct.Parameter.html\" title=\"struct storm_mssql::Parameter\">Parameter</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[173]}
+//{"start":57,"fragment_lengths":[163]}
