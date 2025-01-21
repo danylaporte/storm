623c7dd959c973cb341e61d598c5a43eee3b6f09
danylaporte <danylaporte@users.noreply.github.com>
2025-01-21 14:31:07 +0000

    deploy: a19c7f3798b1437b94cca2a1743afb38dd95da40

--- trait.impl/storm/provider/provider_factory/trait.ProviderFactory.js
+++ trait.impl/storm/provider/provider_factory/trait.ProviderFactory.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["storm_mssql",[["impl ProviderFactory for <a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>"],["impl ProviderFactory for <a class=\"struct\" href=\"storm_mssql/struct.TransactionScoped.html\" title=\"struct storm_mssql::TransactionScoped\">TransactionScoped</a>&lt;<a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>&gt;"]]]]);
+    var implementors = Object.fromEntries([["storm_mssql",[["impl <a class=\"trait\" href=\"storm/provider/provider_factory/trait.ProviderFactory.html\" title=\"trait storm::provider::provider_factory::ProviderFactory\">ProviderFactory</a> for <a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>"],["impl <a class=\"trait\" href=\"storm/provider/provider_factory/trait.ProviderFactory.html\" title=\"trait storm::provider::provider_factory::ProviderFactory\">ProviderFactory</a> for <a class=\"struct\" href=\"storm_mssql/struct.TransactionScoped.html\" title=\"struct storm_mssql::TransactionScoped\">TransactionScoped</a>&lt;<a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[475]}
+//{"start":57,"fragment_lengths":[791]}
