bc194d161d26fd838af9f8e602e38ea27e9a60cd
danylaporte <danylaporte@users.noreply.github.com>
2025-04-08 17:36:44 +0000

    deploy: 59f6873b3bc2e568f6723e74a4a8cda711215299

--- trait.impl/serde/ser/trait.Serialize.js
+++ trait.impl/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["storm",[["impl&lt;Fields&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;<div class=\"where\">where\n    Fields: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["storm",[["impl&lt;Fields&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;<div class=\"where\">where\n    Fields: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
