8baf1f545a0951a249bbdaa744c692938ed99cfd
danylaporte <danylaporte@users.noreply.github.com>
2025-08-21 15:58:06 +0000

    deploy: 269b34fc7b864bf79d4ce91313152d85cceb27b9

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["storm",[["impl&lt;Fields: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;"]]]]);
+    var implementors = Object.fromEntries([["storm",[["impl&lt;Fields: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"storm/enum.FieldsOrStr.html\" title=\"enum storm::FieldsOrStr\">FieldsOrStr</a>&lt;Fields&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
