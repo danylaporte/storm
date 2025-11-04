c78b330bf7ef24a15d189e02e9b8e0c6d82b4f66
danylaporte <danylaporte@users.noreply.github.com>
2025-11-04 16:31:24 +0000

    deploy: dbc7b243e3461ba2a26dc386c28f17b9cad443dd

---
+++ search.index/crateNames/a25945f5dad5.js
@@ -0,0 +1,1 @@
+rd_("estormlstorm_derivekstorm_mssql")
--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["storm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"storm/enum.ApplyOrder.html\" title=\"enum storm::ApplyOrder\">ApplyOrder</a>"]]]]);
+    var implementors = Object.fromEntries([["storm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"storm/enum.ApplyOrder.html\" title=\"enum storm::ApplyOrder\">ApplyOrder</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
