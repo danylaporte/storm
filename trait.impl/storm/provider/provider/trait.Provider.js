e980dcdcdad0e20c7f0ebebc3bc298f96273a314
danylaporte <danylaporte@users.noreply.github.com>
2026-09-23 12:15:29 +0000

    deploy: 9e39972f550a7d6b8d779e6a3a3020e63fedec28

--- trait.impl/storm/provider/provider/trait.Provider.js
+++ trait.impl/storm/provider/provider/trait.Provider.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["storm_mssql",[["impl <a class=\"trait\" href=\"storm/provider/provider/trait.Provider.html\" title=\"trait storm::provider::provider::Provider\">Provider</a> for <a class=\"struct\" href=\"storm_mssql/struct.MssqlProvider.html\" title=\"struct storm_mssql::MssqlProvider\">MssqlProvider</a>",0]]]]);
+    const implementors = Object.fromEntries([["storm_mssql",[["impl Provider for <a class=\"struct\" href=\"storm_mssql/struct.MssqlProvider.html\" title=\"struct storm_mssql::MssqlProvider\">MssqlProvider</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[298]}
+//{"start":59,"fragment_lengths":[170]}
--- trait.impl/storm/provider/provider_factory/trait.ProviderFactory.js
+++ trait.impl/storm/provider/provider_factory/trait.ProviderFactory.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["storm_mssql",[["impl <a class=\"trait\" href=\"storm/provider/provider_factory/trait.ProviderFactory.html\" title=\"trait storm::provider::provider_factory::ProviderFactory\">ProviderFactory</a> for <a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>",0],["impl <a class=\"trait\" href=\"storm/provider/provider_factory/trait.ProviderFactory.html\" title=\"trait storm::provider::provider_factory::ProviderFactory\">ProviderFactory</a> for <a class=\"struct\" href=\"storm_mssql/struct.TransactionScoped.html\" title=\"struct storm_mssql::TransactionScoped\">TransactionScoped</a>&lt;<a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>&gt;",0]]]]);
+    const implementors = Object.fromEntries([["storm_mssql",[["impl ProviderFactory for <a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>",0],["impl ProviderFactory for <a class=\"struct\" href=\"storm_mssql/struct.TransactionScoped.html\" title=\"struct storm_mssql::TransactionScoped\">TransactionScoped</a>&lt;<a class=\"struct\" href=\"storm_mssql/struct.MssqlFactory.html\" title=\"struct storm_mssql::MssqlFactory\">MssqlFactory</a>&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[795]}
+//{"start":59,"fragment_lengths":[479]}
